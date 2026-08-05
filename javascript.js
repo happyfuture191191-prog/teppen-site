8e69694d9020a97ed48af05bbdbaf09f69a827e8
happyfuture191191-prog <happyfuture19191@gmail.com>
2026-08-05 21:39:33 +0900

    all_check

--- javascript.js
+++ javascript.js
@@ -53,13 +53,15 @@ const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightbox-img");
 const closeBtn = document.querySelector(".lightbox .close");
 
-document.querySelectorAll(".lightbox-target").forEach(img => {
-  img.addEventListener("click", () => {
-    lightbox.style.display = "flex";
-    lightboxImg.src = img.src;
+if (lightbox && lightboxImg && closeBtn) {
+  document.querySelectorAll(".lightbox-target").forEach(img => {
+    img.addEventListener("click", () => {
+      lightbox.style.display = "flex";
+      lightboxImg.src = img.src;
+    });
   });
-});
 
-closeBtn.addEventListener("click", () => {
-  lightbox.style.display = "none";
-});
+  closeBtn.addEventListener("click", () => {
+    lightbox.style.display = "none";
+  });
+}
